19ca400ba811e9f03acd1674d02d9a55c405202a
Wim Declerck <wimdeclerck@iMac-van-Wim.home>
2017-02-12 19:46:46 +0100

    onSuccessHandler in getPicure()

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ alert("Hallo iedereen, alles wordt klaargemaakt");
 
 const onDeviceReady = () => {
     alert("Klaar")
-    navigator.camera.getPicture()
+    navigator.camera.getPicture(() =>{})
 };
 
 document.addEventListener("deviceready", onDeviceReady, false);
